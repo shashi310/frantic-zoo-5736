4ac9b26302f5925ec2051b4fbaee5ef6d6bc9159
Shashikant Yadav <103162888+shashi310@users.noreply.github.com>
2023-05-05 00:59:49 +0530

    json

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Restaurent from './component/Restaurent/Restaurent';
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+    <Restaurent />
+    </div>
+  );
+}
+
+export default App;
